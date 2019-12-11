8bfdeb203098ddb14cf475974c89c4232c2ca46a
Student <example@example.com>
2019-12-11 15:06:00 -0800

    add optimized text read and write, search and replace.

--- lib/fr.rb
+++ lib/fr.rb
@@ -1,5 +1,14 @@
 class FR
-  def find_replace(argu1, argue2, string)
-    string.gsub(argu1, argue2)
+
+def write_file(input)
+  File.write("lib/test.txt", input)
+end
+  def find_replace(argu1, argu2)
+    argu1 = argu1.downcase
+    argu2 = argu2.downcase
+    file = File.open("lib/test.txt")
+    string = file.read.downcase
+    string = string.gsub("\n", "")
+    string.gsub(/\b#{argu1}\b/, argu2)
   end
 end
--- spec/fr_spec.rb
+++ spec/fr_spec.rb
@@ -2,10 +2,29 @@ require('rspec')
 require('fr')
 
 describe(FR) do
+
   describe('#find_replace') do
     it("takes in two strings and replaces the all instances of one string with the other in a sentence") do
       test = FR.new()
-      expect(test.find_replace("cat", "dog", "I am walking my cat to the cathedral")).to(eq("I am walking my dog to the doghedral"))
+      expect(test.find_replace("cat", "dog")).to(eq("i am walking my dog to the cathedral"))
+    end
+
+    it("takes in two strings and replaces the all instances of one string with the other in a sentence while ignoring case") do
+      test = FR.new()
+      expect(test.find_replace("cAt", "Dog")).to(eq("i am walking my dog to the cathedral"))
+    end
+    it("takes in two strings and replaces the all instances of one string with the other in a sentence while ignoring case") do
+      test = FR.new()
+      test.write_file("i am walking my cat to the cathedral")
+      expect(test.find_replace("cat", "Dog")).to(eq("i am walking my dog to the cathedral"))
     end
   end
+
+  # describe('#txtfile') do
+  #   it("opens and displays text from file") do
+  #     test = FR.new()
+  #     expect(test.txtfile).to(eq("I am wAlking my cAt to the cAthedRal"))
+  #   end
+  # end
+
 end
